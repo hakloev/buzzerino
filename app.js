db8d67eb52844f625bec5188c01ef8c575def3e3
Håkon Ødegård Løvdal <me@hakloev.no>
2015-11-03 17:07:53 +0100

    debug

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-var sha1 = require('sha1')
-var express = require('express')
+var debug = require('debug')('buzzerino')
+var express = require('express'),
+	bodyParser = require('body-parser'),
+	cookieParser = require('cookie-parser'),
+	nunjucks = require('nunjucks')
 var app = express()
-var bodyParser = require('body-parser')
-var cookieParser = require('cookie-parser')
-var nunjucks = require('nunjucks')
 var server = require('http').createServer(app)
 var io = require('socket.io')(server)
+var sha1 = require('sha1')
 
-var HOST = "0.0.0.0"
-var PORT = 4200
-var PASSWORD = ''
-var ADMIN_TOKEN = sha1(Math.random() + 'admin_token')
+var COOKIE_SECRET = sha1(Math.random() + 'admin_token'),
+	COOKIE_NAME = 'admin_token',
+	HOST = '0.0.0.0',
+	PORT = 4200,
+	ADMIN_PASSWORD = ''
 
 var admin_socket = null
 
@@ -22,22 +24,21 @@ nunjucks.configure('views', {
 
 // Express configuration
 app.use(express.static(__dirname + '/bower_components'))
-app.use(express.static(__dirname + '/public'));
-
+app.use(express.static(__dirname + '/public'))
+app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
-
-app.use(cookieParser())
 
 // Express routes
 app.get('/', function(req, res, next) {
 	res.render('index.html', { title: 'Buzzerino' })
 })
 
-app.get('/buzz', function(req, res, next) {
+app.get('/buzz', function(req, res) {
 	if (!admin_socket) {
+		debug('Tried to access /buzz when no admin connected')
 		res.status(400)
 		res.render('error.html', { error: 'Buzzerino Adminirano has not connected yet' })
 		return
@@ -45,54 +46,64 @@ app.get('/buzz', function(req, res, next) {
 
 	var name = req.query.name
 	if (name === undefined) {
+		debug('No team name supplied, redirecting to /')
 		res.redirect('/')
 		return;
 	}
 	res.render('buzz.html', { title: "Let's go!", name: name } )
 })
 
-app.get('/buzz/admin', function(req, res, next) {
-	if (req.cookies.admin_token != ADMIN_TOKEN) {
-		res.redirect('/buzz/auth')
+app.get('/admin', function(req, res) {
+	if (admin_socket) {
+		debug('Tried to access /admin, but there already exists an admin')
+		res.status(400)
+		res.render('error.html', { error: 'There already exists a Buzzerino Adminirano' })
+		return
+	} 
+
+	if (req.cookies[COOKIE_NAME] != COOKIE_SECRET) {
+		debug(COOKIE_NAME + ' was invalid')
+		res.redirect('/auth')
 		return
 	} 
 	res.render('admin.html', { title: 'Buzzerino Adminirano', teams: teams.sockets.length })
 })
 
-app.get('/buzz/auth', function(req, res, next) {
+app.get('/auth', function(req, res) {
 	res.render('auth.html', { title: 'Admin Authentication' })
 })
 
-app.post('/buzz/auth', function(req, res, next) {
-	if (req.body.password === PASSWORD) {
-		res.cookie('admin_token', ADMIN_TOKEN)
-		res.redirect('/buzz/admin')
+app.post('/auth', function(req, res) {
+	if (req.body.password === ADMIN_PASSWORD) {
+		debug('Wrong password')
+		res.cookie(COOKIE_NAME, COOKIE_SECRET)
+		res.redirect('/admin')
 	} else {
-		res.redirect('buzz//auth')
+		res.redirect('/auth')
 	}
-})
-
-server.listen(PORT, HOST, function() {
-	console.log('Buzzerino-server listening on: ' + HOST + ':' + PORT)
 })
 
 // Socket IO
 var admin = io
 	.of('/admin')
 	.on('connection', function(socket) {
-		console.log('Admin Connected')
+		debug('Admin Connected')
 		if (teams.sockets.length > 0) {
 			teams.emit('admin-reconnect') // activate button, admin reconnect
 		}
-		admin_socket = socket
+		
+		if (!admin_socket) {
+			debug('New admin')
+			admin_socket = socket
+		}
 		
 		socket.on('reset', function(data) {
-			console.log('Admin demanding reset')
+			debug('Admin demanding reset')
 			teams.emit('reset')
 		})
 
 		socket.on('disconnect', function() {
-			console.log('Admin disconnected')
+			debug('Admin disconnected')
 			teams.emit('admin-disconnect')
 			admin_socket = null
 		})
@@ -101,23 +112,26 @@ var admin = io
 var teams = io
 	.of('/teams')
 	.on('connection', function(socket) {
+		debug('Team connected')
 		if (!admin_socket) {
-			console.log('No admin connected yet')
+			debug('No admin connected yet')
 			return
 		}
-		console.log('Team connected')
 		admin.emit('team-connect', { teams: teams.sockets.length })
 
 		socket.on('buzz-from', function(data) {
 			var buzzTime = new Date().getTime()
-			console.log('Team buzzed at ' + buzzTime)
+			debug('Team buzzed at ' + buzzTime)
 			data.time = buzzTime
 			admin.emit('buzz-from', data)
 		})
 
 		socket.on('disconnect', function() {
-			console.log('Team disconnected')
+			debug('Team disconnected')
 			admin.emit('team-disconnect', { teams: teams.sockets.length })
 		})
 	})
 
+server.listen(PORT, HOST, function() {
+	debug('Buzzerino-server listening on: ' + HOST + ':' + PORT)
+})
